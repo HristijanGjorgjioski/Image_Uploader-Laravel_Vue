a9b08a3948ed38ea95ee4acebc0ac0ace30c5fad
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2022-02-28 09:47:11 +0100

    Importing filepond for image upload

---
+++ resources/js/components/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="mt-4">
+    <file-pond
+      name="image"
+      ref="pond"
+      label-idle="Click to choose image, or drag here..."
+      server="/upload"
+      @init="filepondInitialized"
+    />
+  </div>
+</template>
+<script>
+import vueFilePond from "vue-filepond";
+import "filepond/dist/filepond.min.css";
+
+const FilePond = vueFilePond();
+
+export default {
+  components: {
+    FilePond,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    filepondInitialized() {
+      console.log('filepondInitialized is ready)
+
+    }
+  }
+};
+</script>
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\ImageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ImageController::class, 'index']);
+// Route::get('/', 'Hello');
 Route::get('/images', [ImageController::class, 'show']);
 Route::post('/upload', [ImageController::class, 'store']);
